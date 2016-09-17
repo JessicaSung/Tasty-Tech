d3a926d10552ca231d9b578b6262fd4b4460661b
JessicaSung <jessica.sung@utexas.edu>
2016-09-17 13:18:36 -0500

    added basic bootstrap template. added logo

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB_VPm_7dN7gNGOUqyvQtlpo1EmcWrpvcU",
+    authDomain: "tasty-tech-f4e06.firebaseapp.com",
+    databaseURL: "https://tasty-tech-f4e06.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "325466871965"
+  };
+  firebase.initializeApp(config);
+
+  // VARIABLES
+	var database = firebase.database();
