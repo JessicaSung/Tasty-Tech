b67772c40bee68f1ac49c60795e9a500de697c79
JessicaSung <jessica.sung@utexas.edu>
2016-09-17 14:04:12 -0500

    added code for ingredients list similar to 'todo in-class activity'

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,70 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyB_VPm_7dN7gNGOUqyvQtlpo1EmcWrpvcU",
-    authDomain: "tasty-tech-f4e06.firebaseapp.com",
-    databaseURL: "https://tasty-tech-f4e06.firebaseio.com",
-    storageBucket: "",
-    messagingSenderId: "325466871965"
-  };
-  firebase.initializeApp(config);
+// Make a ingredients list
+var ingredientCount = 0;
 
-  // VARIABLES
-	var database = firebase.database();
+	// Display user ingredients to page
+	$("#addIngredient").on("click", function(){
+
+		// get the ingredient "value" from the textbox
+		var ingredientTask = $('#ingredient').val().trim();
+
+		// display user input in a paragraph
+		var ingredientItem = $('<p>');
+		ingredientItem.attr("id", "item-" + ingredientCount);
+		ingredientItem.append(" " + ingredientTask);
+
+		// create a button that can be clicked to delete ingredient
+		var ingredientClose = $("<button>");
+		ingredientClose.attr("data-ingredient", ingredientCount);
+		ingredientClose.addClass("checkbox");
+		ingredientClose.append("X");
+
+		// add the X button in front of the user input paragraph
+		ingredientItem = ingredientItem.prepend(ingredientClose);
+
+		// add the button and paragraph to the page
+		$("#main").append(ingredientItem);
+
+		// clear the textbox when done
+		$('#main').val("");
+
+		// increment the the todoCount
+		ingredientCount++;
+
+		// Prevent Form from Refreshing (return false)
+	return false;
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Initialize Firebase
+// var config = {
+// apiKey: "AIzaSyB_VPm_7dN7gNGOUqyvQtlpo1EmcWrpvcU",
+// authDomain: "tasty-tech-f4e06.firebaseapp.com",
+// databaseURL: "https://tasty-tech-f4e06.firebaseio.com",
+// storageBucket: "",
+// messagingSenderId: "325466871965"
+// };
+// firebase.initializeApp(config);
+
+// VARIABLES
+// var database = firebase.database();
+
+
+
+
+
+
+
+// AJAX call
