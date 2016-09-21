f0adbd67ab4b063c5ef9be6eeead11181a7fe0d2
JessicaSung <jessica.sung@utexas.edu>
2016-09-20 19:21:35 -0500

    added AJAX call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,90 @@
-// Make a ingredients list
-var ingredientCount = 0;
+// AJAX call on one ingredient
+$(document).on('click', '#addIngredient', function(){
+	var ingredient = $('#ingredient').val().trim();
+	var key = "3ddf33388a85595eb6bcc15116a16e16";
+	var queryURL = "http://food2fork.com/api/search?key=" + key + "&q=" + ingredient;
 
-	// Display user ingredients to page
-	$("#addIngredient").on("click", function(){
+	console.log(ingredient);
+	console.log(key);
 
-		// get the ingredient "value" from the textbox
-		var ingredientTask = $('#ingredient').val().trim();
+	  $.ajax({
+            url: queryURL,
+            method: 'GET'
+        })
+        .done(function(response) {
+            
 
-		// display user input in a paragraph
-		var ingredientItem = $('<p>');
-		ingredientItem.attr("id", "item-" + ingredientCount);
-		ingredientItem.append(" " + ingredientTask);
+            console.log(queryURL);
 
-		// create a button that can be clicked to delete ingredient
-		var ingredientClose = $("<button>");
-		ingredientClose.attr("data-ingredient", ingredientCount);
-		ingredientClose.addClass("checkbox");
-		ingredientClose.append("X");
+            console.log(response);
 
-		// add the X button in front of the user input paragraph
-		ingredientItem = ingredientItem.prepend(ingredientClose);
-
-		// add the button and paragraph to the page
-		$("#list").append(ingredientItem);
-
-		// clear the textbox when done
-		$('#list').val("");
-
-		// increment the the todoCount
-		ingredientCount++;
-
-		// Prevent Form from Refreshing (return false)
-	return false;
-
+            var results = response.data;
+           
+      	});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Make a ingredients list
+// var ingredientCount = 0;
+
+// 	// Display user ingredients to page
+// 	$("#addIngredient").on("click", function(){
+
+// 		// get the ingredient "value" from the textbox
+// 		var ingredientTask = $('#ingredient').val().trim();
+
+// 		// display user input in a paragraph
+// 		var ingredientItem = $('<p>');
+// 		ingredientItem.attr("id", "item-" + ingredientCount);
+// 		ingredientItem.append(" " + ingredientTask);
+
+// 		// create a button that can be clicked to delete ingredient
+// 		var ingredientClose = $("<button>");
+// 		ingredientClose.attr("data-ingredient", ingredientCount);
+// 		ingredientClose.addClass("checkbox");
+// 		ingredientClose.append("X");
+
+// 		// add the X button in front of the user input paragraph
+// 		ingredientItem = ingredientItem.prepend(ingredientClose);
+
+// 		// add the button and paragraph to the page
+// 		$("#list").append(ingredientItem);
+
+// 		// clear the textbox when done
+// 		$('#list').val("");
+
+// 		// increment the the todoCount
+// 		ingredientCount++;
+
+// 		// Prevent Form from Refreshing (return false)
+// 	return false;
+
+// });
+
+
 
 
 
